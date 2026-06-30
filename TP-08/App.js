e16be0390f25e45f5c2574f0a3e9cdc17f84e3f1
Android Studio <devandroid@ort>
2026-06-30 10:08:12 -0300

    haciendo estructura

--- TP-08/App.js
+++ TP-08/App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+//import { NavigationContainer } from '@react-navigation/native'
+//import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Home from './componentes/Home';
+import Detalle from './componentes/Detalle';
+import Perfil from './componentes/Perfil';
+
+const Stack = createNativeStackNavigator()
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detalle} />
+        <Stack.Screen name="Profile" component={Perfil} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
