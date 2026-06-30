7b4f3ea48d1b3805284eac98461db6bcbae4e76d
Android Studio <devandroid@ort>
2026-06-30 10:14:00 -0300

    arreglando problemas de dependencia

--- TP-08/App.js
+++ TP-08/App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler'
 
-//import { NavigationContainer } from '@react-navigation/native'
-//import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import Home from './componentes/Home';
-import Detalle from './componentes/Detalle';
-import Perfil from './componentes/Perfil';
+import Home from './componentes/Home'
+import Detalle from './componentes/Detalle'
+import Perfil from './componentes/Perfil'
 
 const Stack = createNativeStackNavigator()
 
