ae6b5b386939a92f6c255c0fec2f6a5ff0382382
Android Studio <devandroid@ort>
2026-06-30 10:26:20 -0300

    haciendo feed

--- TP-08/componentes/Home.js
+++ TP-08/componentes/Home.js
@@ -1,3 +1,36 @@
-export default function Home() {
+import { View, Text, FlatList, StyleSheet } from 'react-native'
+import Publicacion from '../Publicacion/Publicacion'
 
+export default function Feed({ Publicaciones, onSelect, toggleLike }) {
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>TRENDING</Text>
+
+      <FlatList
+        data={Publicaciones}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Publicacion
+            publicacion={item}
+            onSelect={() => onSelect(item.id)}
+            toggleLike={toggleLike}
+          />
+        )}
+      />
+
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10
+  }
+})
